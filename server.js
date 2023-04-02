d90c0b17a21d69c6517530ebc1fb6d5a098b9414
showanne <showanne.e@gmail.com>
2023-04-03 01:17:18 +0800

    Add：環境變數 (更新資料庫雲端連線位置)

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const Post = require('./models/post');
+const dotenv = require('dotenv');
 
+dotenv.config({path:"./config.env"}); // 使用環境變數 process.env.Name
+
+// 將 DB 資料及密碼替換成環境參數
+const DB = process.env.Database.replace(
+    '<password>', process.env.Database_pwd
+)
 
 // :27017 是 mongoDB 預設 port
 // /testPost 是 mongoDB 資料庫名稱
 mongoose
-.connect("mongodb://localhost:27017/testPost")
+.connect(DB)
 .then(() => console.log('資料庫連接成功'));
 
 const requestListener = async(req, res)=>{
@@ -89,4 +96,4 @@ const requestListener = async(req, res)=>{
 
 // 開啟伺服器
 const server = http.createServer(requestListener);
-server.listen(3000);
+server.listen(process.env.PORT);
